5ef2de82828eeee130388e3f5bb4e8772befe845
Seth Messer <seth.messer@gmail.com>
2021-01-05 11:26:24 -0600

    updated docs with ascii representation

--- keyboards/atreus62/keymaps/megalithic/keymap.c
+++ keyboards/atreus62/keymaps/megalithic/keymap.c
@@ -13,6 +13,8 @@
 
 #define MG_CTRL_ESC CTL_T(KC_ESC)  // Tap for Escape, hold for Control
 #define MG_F_FN     LT(_FN, KC_F)
+#define MG_WB       A(KC_LEFT) // FIXME
+#define MG_WF       A(KC_RIGHT) // FIXME
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* qwerty */
@@ -20,32 +22,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS ,
     MG_CTRL_ESC,    KC_A,    KC_S,    KC_D,    MG_F_FN, KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC ,
-    MO(_FN),        KC_LBRC, KC_LALT, KC_LGUI, MO(_FN), KC_BSPC, KC_F19,   KC_ENT,      KC_SPC,  KC_EQL,  KC_MINS, KC_QUOT, KC_RBRC, KC_RGUI
+    MO(_FN),        KC_LBRC, KC_LALT, KC_LGUI, MO(_FN), KC_BSPC, KC_F19,   KC_ENT,      KC_SPC,  KC_SPC,  KC_EQL,  KC_DEL,  KC_RBRC, KC_RGUI
   ),
 
   [_FN] = LAYOUT(
-    TO(_DEFAULT),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL  ,
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_F12,  KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS ,
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS ,
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
+    KC_TRNS,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL  ,
+    KC_TRNS,        KC_TRNS, MG_WF  , KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS ,
+    TO(_DEFAULT),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS ,
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MG_WB  ,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
     KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(_RESET),  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_RESET] = LAYOUT(
-    TO(_DEFAULT),   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    TO(_DEFAULT)  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , RESET
   )
-
-  /*
-  [_TRNS] = LAYOUT(
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-  */
 };
