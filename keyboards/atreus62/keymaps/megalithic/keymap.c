2e29c14631fcf9ffd3d0a2820df1de66b71df7bb
Seth Messer <seth.messer@gmail.com>
2021-01-07 09:40:30 -0600

    More attempts at correcting optimal layout and such; added new combo

--- keyboards/atreus62/keymaps/megalithic/keymap.c
+++ keyboards/atreus62/keymaps/megalithic/keymap.c
@@ -13,7 +13,6 @@
 
 #define MG_CTRL_ESC CTL_T(KC_ESC)  // Tap for Escape, hold for Control
 #define MG_F_FN     LT(_FN, KC_F)
-/* #define MG_ENT_F19  MT(KC_F19, KC_ENT) */
 #define MG_FN       MO(_FN)
 #define MG_RESET    MO(_RESET)
 #define MG_WB       A(KC_LEFT) // FIXME - presently not working as we'd expect
@@ -25,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS ,
     MG_CTRL_ESC,    KC_A,    KC_S,    KC_D,    MG_F_FN, KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC ,
-    MG_FN,          KC_LBRC, KC_TRNS, KC_LALT, KC_LGUI, MG_FN, KC_F19,     KC_ENT,      KC_BSPC, KC_SPC,  KC_EQL,  KC_DEL,  KC_RBRC, KC_RGUI
+    MG_FN,          KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, MG_FN, KC_F19,     KC_ENT,      KC_SPC,  KC_BSPC,  KC_DEL,  KC_TRNS, KC_TRNS, KC_RGUI
   ),
 
   [_FN] = LAYOUT(
@@ -33,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,        KC_TRNS, MG_WF  , KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS ,
     TO(_DEFAULT),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS ,
     KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MG_WB  ,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MG_RESET,    KC_DEL , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MG_RESET,    KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_RESET] = LAYOUT(
@@ -43,4 +42,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , RESET
   )
+};
+
+
+// -- COMBOS:
+//
+enum combos {
+    CTRL_BSPC_DEL
+};
+
+const uint16_t PROGMEM ctrl_bspc_del_combo[] = {KC_LCTL, KC_BSPC, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    [CTRL_BSPC_DEL] = COMBO(ctrl_bspc_del_combo, KC_DEL)
 };
