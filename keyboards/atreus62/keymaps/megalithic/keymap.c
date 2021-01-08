50100b292974d713b5758474761b323e5270c733
Seth Messer <seth.messer@gmail.com>
2021-01-08 10:58:18 -0600

    ditch space cadet shift for now

--- keyboards/atreus62/keymaps/megalithic/keymap.c
+++ keyboards/atreus62/keymaps/megalithic/keymap.c
@@ -20,19 +20,19 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* qwerty */
-    KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
-    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS ,
-    MG_CTRL_ESC,    KC_A,    KC_S,    KC_D,    MG_F_FN, KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
-    KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC ,
+    KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS ,
+    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS ,
+    MG_CTRL_ESC,    KC_A,    KC_S,    KC_D,    MG_F_FN, KC_G,                           KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
+    KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
     MG_FN,          KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, MG_FN, KC_F19,     KC_ENT,      KC_SPC,  KC_BSPC,  KC_DEL,  KC_TRNS, KC_TRNS, KC_RGUI
   ),
 
   [_FN] = LAYOUT(
-    KC_TRNS,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL  ,
-    KC_TRNS,        KC_TRNS, MG_WF  , KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS ,
-    TO(_DEFAULT),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS ,
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MG_WB  ,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MG_RESET,    KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_EQL  ,
+    KC_TRNS,        KC_TRNS, MG_WF  , KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS,  KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS ,
+    TO(_DEFAULT),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS ,
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MG_WB  ,                        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MG_RESET,    KC_TRNS, KC_DEL,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_RESET] = LAYOUT(
