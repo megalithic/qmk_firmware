898d15a732e33551a464c91444c49a8411abfea1
Seth Messer <seth.messer@gmail.com>
2021-01-09 20:36:30 -0600

    went back to prev layout

--- keyboards/atreus62/keymaps/megalithic/keymap.c
+++ keyboards/atreus62/keymaps/megalithic/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS ,
     MG_CTRL_ESC,    KC_A,    KC_S,    KC_D,    MG_F_FN, KC_G,                           KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    MG_FN,          KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, MG_FN, KC_F19,     KC_ENT,      KC_SPC,  KC_SPC,   KC_DEL,  KC_TRNS, KC_TRNS, KC_RGUI
+    MG_FN,          KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, MG_FN, KC_F19,     KC_ENT,      KC_SPC,  KC_BSPC,  KC_DEL,  KC_TRNS, KC_TRNS, KC_RGUI
   ),
 
   [_FN] = LAYOUT(
@@ -32,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,        KC_TRNS, MG_WF  , KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS,  KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS ,
     TO(_DEFAULT),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS ,
     KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MG_WB  ,                        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MG_RESET,    KC_BSPC, KC_BSPC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MG_RESET,    KC_TRNS, KC_DEL,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_RESET] = LAYOUT(
-    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    TO(_DEFAULT)  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,     KC_NO  , KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , RESET
+    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    TO(_DEFAULT)  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , RESET
   )
 };
 
