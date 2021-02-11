712d06c354f1b65bb0a9085bb375aa8001f17f83
Seth Messer <seth.messer@gmail.com>
2021-02-11 08:39:09 -0600

    adds layer tap for space and L2

--- keyboards/atreus62/keymaps/megalithic/keymap.c
+++ keyboards/atreus62/keymaps/megalithic/keymap.c
@@ -14,17 +14,18 @@
 #define MG_CTRL_ESC CTL_T(KC_ESC)  // Tap for Escape, hold for Control
 #define MG_F_FN     LT(_FN, KC_F)
 #define MG_FN       MO(_FN)
+#define MG_SPC_FN   LT(_FN, KC_SPC)
 #define MG_RESET    MO(_RESET)
 #define MG_WB       A(KC_LEFT) // FIXME - presently not working as we'd expect
 #define MG_WF       A(KC_RIGHT) // FIXME - presently not working as we'd expect
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_DEFAULT] = LAYOUT( /* qwerty */
+  [_DEFAULT] = LAYOUT(
     KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS ,
     KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS ,
     MG_CTRL_ESC,    KC_A,    KC_S,    KC_D,    MG_F_FN, KC_G,                           KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    MG_FN,          KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, MG_FN, KC_F19,     KC_ENT,      KC_SPC,  KC_BSPC,  KC_DEL,  KC_TRNS, KC_TRNS, KC_RGUI
+    MG_FN,          KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, MG_SPC_FN, KC_F19, KC_ENT,      KC_SPC,  KC_BSPC,  KC_DEL,  KC_TRNS, KC_TRNS, KC_RGUI
   ),
 
   [_FN] = LAYOUT(
